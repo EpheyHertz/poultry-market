0d545edd2fd6ad8b6cc3cd76de7e4ce3511c3950
EpheyNyaga <epheynyaga@gmail.com>
2025-10-13 10:14:55 +0300

    `Removed hardcoded LIPIA_API_KEY from test-lipia-online-status.js`

--- test-lipia-online-status.js
+++ test-lipia-online-status.js
@@ -1,5 +1,5 @@
 const checkPaymentStatus = async (transactionReference) => {
-    const LIPIA_API_KEY="b5cb3e7f3f9359bf029e6bd86ad8145d86591375"
+   
   try {
     const response = await fetch(
       `https://lipia-api.kreativelabske.com/api/v2/payments/status?reference=${transactionReference}`,
